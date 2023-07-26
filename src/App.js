f7977ebcdbce42d6f00ad4ac8c572776599118c9
victorvictoria-maker <victorvictoria0001@gmail.com>
2023-07-26 15:01:49 +0100

    Learnt about component and props in react

--- src/App.js
+++ src/App.js
@@ -8,10 +8,30 @@ function App() {
     <div className="App">
       {title}
       <p>My name is {name}.</p>
+
+      <hr/>
+      <User name="Victoria" age={21} email="victoria@gmail.com"/>
+      <User name="Peter" age={19} email="peter@gmail.com"/>
+
+      <hr/>
+      <Job company="Amazon" position="Senior SDE" salary={90000}/>
+      <Job company="Netflix" position="Project Manager" salary={100000}/>
     </div>
   );
 }
 
+const User = (props) => {
+  return (
+    <div>
+      <p>First name is {props.name}</p>
+      <p>Age is {props.age}</p>
+      <p>30 under 30 woman</p>
+    </div>
+  );
+};
 
+const Job = (props) => {
+  return <div>My position at {props.company} is {props.position} with {props.salary} salary.</div>
+};
 
 export default App;
